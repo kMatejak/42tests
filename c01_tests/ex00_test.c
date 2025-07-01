cb4792daa23e75904cfebe2e7d0caa8518794d51
Krzysztof Matejak <kmatejak@c3r13s2.42warsaw.pl>
2025-07-01 22:21:07 +0200

    c01_ex00: added test

---
+++ c01_tests/ex00_test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "../../c01/ex00/ft_ft.c"
+
+int	main(void)
+{
+	int 	i;
+	int*	nbr;
+
+	i = 33;
+	printf("ex00\n");
+	printf("Before: %d\n", i); 
+	nbr = &i;
+	ft_ft(nbr);
+	printf("After: %d\n", i);
+	if (i == 42)
+	{
+		printf("-------- OK\n");
+	}
+	else
+	{
+		printf("-------- KO :(\n");
+	}
+	printf("\n");
+
+	return (1);
+}
