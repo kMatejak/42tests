8e0d3ce32f5185b1c7beaf98e66e3c0bcc409048
Krzysztof Matejak <kmatejak@c3r13s2.42warsaw.pl>
2025-07-10 17:23:52 +0200

    test_c02_ex00: RED testing with standard strcpy

--- c02_tests/test_ex00.c
+++ c02_tests/test_ex00.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "../../c02/ex00/ft_strcpy.c"
 #define RED	"\033[31m"
 #define GREEN	"\033[32m"
@@ -6,22 +7,24 @@
 
 int	main(void)
 {
-	char*	dest;
-	char*	src;
-
-        dest = "yollo";
-	src = "Hello pomello!";
+	char	src[] = "hello pomello!";
+	char	dest[15];
+	//char*	src = &source;
+	//char*	dest = &final;
 
         printf("c02 ex00\n");
         printf("BEFORE\n");
-	printf("Source: %s\nDestination: %s\n", src, dest);
+	printf("Source string: %s\n", src);
 	printf("\n");
 
-	ft_strcpy(dest, src);
-
+	//ft_strcpy(dest, src);
+	strcpy(dest, src);
 	printf("AFTER\n");
+	printf("Copied string with strcpy: %s\n", dest);
+
+	/*
 	printf("Source: %s\nDestination: %s\n", src, dest);
-	
+	*/	
 	printf("\n\n\n");
 	return (1);
 }
