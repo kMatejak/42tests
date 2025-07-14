d26f7c83f6868344aec3134fbf3df460622e56c4
Krzysztof Matejak <kmatejak@c3r13s2.42warsaw.pl>
2025-07-14 17:46:20 +0200

    test_c02_ex00: RED

--- c02_tests/test_ex00.c
+++ c02_tests/test_ex00.c
@@ -7,24 +7,29 @@
 
 int	main(void)
 {
-	char	src[] = "hello pomello!";
-	char	dest[15];
-	//char*	src = &source;
-	//char*	dest = &final;
+	char	src_1[] = "hello pomello!";
+	char	src_2[] = "!@87sjka9;;][\\0";
+	char	src_3[] = "aplm^&_ noob02!";
+	char	dest_1[15];
+	char	dest_2[15];
+	char	dest_strcpy[15];
 
         printf("c02 ex00\n");
         printf("BEFORE\n");
-	printf("Source string: %s\n", src);
+	printf("Source string 1: %s\n", src_1);
+	printf("Source string 2: %s\n", src_2);
+	printf("Source string 3: %s\n", src_3);
 	printf("\n");
 
-	//ft_strcpy(dest, src);
-	strcpy(dest, src);
+	ft_strcpy(dest_1, src_1, 15);
+	ft_strcpy(dest_2, src_2, 15);
+	ft_strcpy(dest_strcpy, src_3, 15);
+	
 	printf("AFTER\n");
-	printf("Copied string with strcpy: %s\n", dest);
+	printf("Copied string 1 with ft_strcpy: %s\n", dest_1);
+	printf("Copied string 2 with ft_strcpy: %s\n", dest_2);
+	printf("strcpy string 3:                %s\n", dest_strcpy);
 
-	/*
-	printf("Source: %s\nDestination: %s\n", src, dest);
-	*/	
 	printf("\n\n\n");
 	return (1);
 }
