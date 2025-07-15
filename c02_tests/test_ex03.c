d43115c3b8b17681850f3ec74577ada6399a43c4
Krzysztof Matejak <kmatejak@c3r13s2.42warsaw.pl>
2025-07-15 19:56:52 +0200

    test_c02_ex03: GREEN

--- c02_tests/test_ex03.c
+++ c02_tests/test_ex03.c
@@ -14,12 +14,12 @@ int	main(void)
 
 	int	a, b, c, d;
 
-        printf("c02 ex01\n");
+        printf("c02 ex03\n");
         printf("BEFORE\n");
-	printf("Source string 1: %s\n", str_a);
-	printf("Source string 2: %s\n", str_b);
-	printf("Source string 3: %s\t(is empty)\n", str_c);
-	printf("Source string 4: %s\n", str_d);
+	printf("Source string 1: >>%s<<\n", str_a);
+	printf("Source string 2: >>%s<<\n", str_b);
+	printf("Source string 3: >>%s<< (is empty)\n", str_c);
+	printf("Source string 4: >>%s<<\n", str_d);
 	printf("\n");
 
 	a = ft_str_is_numeric(str_a);
@@ -30,20 +30,20 @@ int	main(void)
 	printf("AFTER\n");
 	if (a == 0)
 	{
-		printf(GREEN "String a: %s\ndoes not have any numerics." RESET "\n\n", str_a);
+		printf(GREEN "String a: >>%s<<\ndoes not have any numerics." RESET "\n\n", str_a);
 	}
 	else
 	{
-		printf(RED "String a: %s\ndoes not have any numerics." RESET "\n\n", str_a);
+		printf(RED "String a: >>%s<<\ndoes not have any numerics." RESET "\n\n", str_a);
 	}
 
 	if (b == 0)
 	{
-		printf(GREEN "String b: %s\nhas not only numerics." RESET "\n\n", str_b);
+		printf(GREEN "String b: >>%s<<\nhas not only numerics." RESET "\n\n", str_b);
 	}
 	else
 	{
-		printf(RED "String b: %s\nhas not only numerics." RESET "\n\n", str_b);
+		printf(RED "String b: >>%s<<\nhas not only numerics." RESET "\n\n", str_b);
 	}
 
 	if (c == 1)
@@ -57,11 +57,11 @@ int	main(void)
 
 	if (d == 1)
 	{
-		printf(GREEN "String d: %s\ncontains only numerics." RESET "\n\n", str_d);
+		printf(GREEN "String d: >>%s<<\ncontains only numerics." RESET "\n\n", str_d);
 	}
 	else
 	{
-		printf(RED "String d: %s\ncontains only numerics." RESET "\n\n", str_d);
+		printf(RED "String d: >>%s<<\ncontains only numerics." RESET "\n\n", str_d);
 	}	
 	printf("\n\n\n");
 	return (1);
