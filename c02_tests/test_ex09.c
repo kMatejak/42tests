0f2580f9fd4cd6db326acf4295cf09a162a32b78
Krzysztof Matejak <kmatejak@c3r13s2.42warsaw.pl>
2025-07-17 18:57:20 +0200

    test_c02_ex09: GREEN

--- c02_tests/test_ex09.c
+++ c02_tests/test_ex09.c
@@ -8,21 +8,28 @@
 int	main(void)
 {
 	char	str_1[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
-	char	str_2[] = "Workers of the world unite; you have nothing to lose but your chains.";
+	char	str_2[] = "WORkers of the WORld unite; you have nothing to lose but your chains.";
+	char	str_3[] = "lORe 42bee bee";
 
         printf("c02 ex09\n");
         printf("BEFORE\n");
 	printf("Source string 1: %s\n", str_1);
 	printf("Source string 2: %s\n", str_2);
+	printf("Source string 3: %s\n", str_3);
 	printf("\n");
 	
 	printf("AFTER\n");
 	printf("String 1 should be: >>Salut, Comment Tu Vas ? 42mots ");
 	printf("Quarante-Deux; Cinquante+Et+Un<<\n");
 	printf("And it looks like:  >>%s<<\n\n", ft_strcapitalize(str_1));
+
 	printf("String 2 should be: >>Workers Of The World Unite; ");
 	printf("You Have Nothing To Lose But Your Chains.<<\n");
-	printf("And it looks like:  >>%s<<\n", ft_strcapitalize(str_2));
+	printf("And it looks like:  >>%s<<\n\n", ft_strcapitalize(str_2));
+	
+	printf("String 3 should be: >>Lore 42bee Bee<<\n");
+	printf("And it looks like:  >>%s<<\n\n", ft_strcapitalize(str_3));
+	
 	printf("\n\n\n\n");
 	return (1);
 }
